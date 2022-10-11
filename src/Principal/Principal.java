50a2db6a28c6e0c451cd342464258eed28c8d772
kinzasb <yazidslafti7@gmail.com>
2022-10-11 10:26:12 +0200

    Como crear un archivo

---
+++ src/Principal/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+import manejodearchivos.ManejodeArchivos;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Principal {
+    
+        public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String nombreArchivo = "prueba.txt";
+        ManejodeArchivos.crearArchivo(nombreArchivo);
+        
+    }
+    
+}
--- src/manejodearchivos/ManejodeArchivos.java
+++ src/manejodearchivos/ManejodeArchivos.java
@@ -4,6 +4,12 @@
  */
 package manejodearchivos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Alumno Mañana
@@ -13,11 +19,18 @@ package manejodearchivos;
  */
 public class ManejodeArchivos {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   
+
+    
+    public static void crearArchivo(String nombre){
+        
+        File archivo = new File(nombre);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out); 
+        }
     }
     
 }
